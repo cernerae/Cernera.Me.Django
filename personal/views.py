25ff75f1334f3b97ffe18380611d59a6a35c2691
cernerae <cernerae@gmail.com>
2019-06-22 15:30:43 +0000

    Fixed signup 500 error

--- personal/views.py
+++ personal/views.py
@@ -34,7 +34,7 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            auth_login(request, user)
             return redirect('/')
     else:
         form = SignUpForm()
